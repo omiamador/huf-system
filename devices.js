de9dc6b1369efc9c3e2e27566ec157ded84beed4
omiamador <omipibebb@gmail.com>
2026-01-09 01:16:51 -0600

    Add files via upload

---
+++ devices.js
@@ -0,0 +1,63 @@
+/**
+ * HUF System Hub - Configuración de Dispositivos (HIMs)
+ * Basado en el inventario de cámaras y equipos de prueba - HUF México.
+ * Desarrollado por Omar Amador
+ */
+
+window.huf_devices_config = [
+    {
+        id: "1",
+        name: "Chambers Thermotron SE-1000-6-5",
+        hims: ["HIM-1138", "HIM-0941", "HIM-2664", "HIM-2662", "HIM-0916", "HIM-0917", "HIM-1139", "HIM-2663"]
+    },
+    {
+        id: "2",
+        name: "Chambers SM-16-2-0",
+        hims: ["HIM-0939", "HIM-0938", "HIM-1144", "HIM-0943", "HIM-2660", "HIM-1143", "HIM-2675", "HIM-2682", "HIM-2656", "HIM-2659", "HIM-2666", "HIM-2668", "HIM-2669"]
+    },
+    {
+        id: "3",
+        name: "Thermal Shock",
+        hims: ["HIM-2661", "HIM-1137"]
+    },
+    {
+        id: "4",
+        name: "Durability Car Lockset Servo",
+        hims: ["HIM-0363", "HIM-0362", "HIM-0674", "HIM-2717", "HIM-0675"]
+    },
+    {
+        id: "5",
+        name: "Durability Car Lockset BR",
+        hims: ["HIM-2782"]
+    },
+    {
+        id: "6",
+        name: "Durability Car Bracket",
+        hims: ["HIM-0672", "HIM-0673", "HIM-2673", "HIM-2674", "HIM-2723", "HIM-2724", "HIM-2651"]
+    },
+    {
+        id: "7",
+        name: "Durability Car Jailor",
+        hims: ["HIM-3139", "HIM-3258", "HIM-3584"]
+    },
+    {
+        id: "8",
+        name: "Cyclic Corrosion",
+        hims: ["HIM-2653"]
+    },
+    {
+        id: "9",
+        name: "Vibration",
+        hims: ["HIM-3483"]
+    },
+    {
+        id: "10",
+        name: "Cross Section",
+        hims: ["HIM-1006"]
+    },
+    {
+        id: "11",
+        name: "Salt Spray",
+        hims: ["HIM-1146"]
+    }
+];
