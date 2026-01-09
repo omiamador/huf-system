fef2998c59d20db97fffcec0ece5d07ff9863740
omiamador <omipibebb@gmail.com>
2026-01-08 21:43:31 -0600

    Add files via upload

---
+++ devices.js
@@ -0,0 +1,32 @@
+// DEVICES.js
+window.huf_devices_config = [
+    {
+        id: "1",
+        name: "Climatic Chambers-CART",
+        hims: ["HIM-0112", "HIM-0222", "HIM-0322", "HIM-0422"],
+    },
+    {
+        id: "2",
+        name: "Climatic Chambers-SMALL",
+        hims: ["VIB-A", "VIB-B"],
+
+    },
+    {
+        id: "3",
+        name: "JAILOR",
+        hims: ["OV-01", "OV-02", "OV-03"],
+
+    },
+    {
+        id: "4",
+        name: "JAILOCK",
+        hims: ["SS-01"],
+
+    },
+	    {
+        id: "5",
+        name: "VIBRATION",
+        hims: ["SS-01"],
+
+    }
+];
